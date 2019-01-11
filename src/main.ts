9ae9668fa5fbfdb3c93013c50db619f99d0f3fb4
sorasful <teva.krief@hotmail.fr>
2019-01-11 10:27:12 +0100

    Starting add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { configService } from './config/config.service';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const options = new DocumentBuilder()
+    .setTitle('Blog de ouf')
+    .setDescription("Blog's swagger.")
+    .setVersion('0.1')
+    .addTag('Blog')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(configService.getNumber('PORT'));
 }
 bootstrap();
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,11 +1,15 @@
-import { Body, Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Get, HttpStatus, Param } from '@nestjs/common';
 import { UserService } from './user.service';
+import {ApiUseTags, ApiResponse} from '@nestjs/swagger';
 
+@ApiUseTags('User')
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Get(':id')
+  @ApiResponse({status : HttpStatus.OK, description:'User found and retrieved'} )
+  @ApiResponse({status : HttpStatus.NOT_FOUND, description:'User not found'})
   async getById(@Param('id') id: string) {
     return this.userService.getById(id);
   }
